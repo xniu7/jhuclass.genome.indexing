e3dbf482f8404df283e77754ca126048791aafbf
xniu7 <xniu7@jhu.edu>
2014-05-30 14:43:56 -0400

    add file_type(fasta,fastq)

--- code/sort.py
+++ code/sort.py
@@ -29,39 +29,69 @@ def getSC(master, name):
     return sc
 
 # select a sort method
-def sort(sort_name, reads, cores_num):
+def sort(sort_name, reads, threads_number):
     if (sort_name=='radix'):
         bwt = radixSort(reads)
     elif (sort_name=='segment'):
         bwt = segSort(reads)
     elif (sort_name=='partition'):
-        bwt = partitionSort(reads, cores_num)
+        bwt = partitionSort(reads, threads_number)
     else:
-        bwt = defaultSort(reads, cores_num)
+        bwt = defaultSort(reads, threads_number)
     return bwt
 
+# RDD does not support communications among lines, 
+# because each line is independent during processing.
+# Thus we first collect RDD (RDD->List), then parallelize List (List->RDD)
+def getReads(lines, file_type):
+    #concatinate lines begin with '>'
+    if file_type == 'fasta' :
+        # drop lines with '>'
+        #reads = lines.filter(lambda line: '>' not in line)
+        reads = []
+        read = ''
+        lines = lines.collect()
+        for line in lines : 
+            if '>' not in line:
+                read += line
+            else :
+                if len(read)>0: reads.append(read) 
+                read = ''
+        if len(read)>0: reads.append(read) 
+    #choose the second line of every four lines            
+    elif file_type == 'fastq' :
+        # drop lines with '@' '+'
+        reads = lines.collect()[1::4]
+    else :
+        reads = lines.collect()
+    return reads
+
 if __name__ == "__main__":
-    if len(sys.argv) < 6:
-        print >> sys.stderr, "Usage: <sort> <master> <threads_num> <input> <output>"
+    if len(sys.argv) < 7:
+        print >> sys.stderr, "Usage: <sort> <master> <threads_num> <file_type> <input> <output>"
         exit(-1)
     start_time = datetime.now()
     
     sort_method = sys.argv[1]
     master_address = sys.argv[2]
     threads_number = sys.argv[3]
-    input_path = sys.argv[4]
-    output_path = sys.argv[5]
+    file_type = sys.argv[4]
+    input_path = sys.argv[5]
+    output_path = sys.argv[6]
     
     sc = getSC(master_address, sort_method+threads_number+input_path)
 
-    # drop lines with '>'
-    reads = sc.textFile(input_path,int(threads_number)).filter(lambda s: '>' not in s)
+    lines = sc.textFile(input_path,int(threads_number))
+    # collect RDD (RDD->List)
+    reads = getReads(lines,file_type)
+    # parallelize List (List->RDD)
+    reads = sc.parallelize(reads,int(threads_number))
     # sort suffixes
     bwt = sort(sort_method,reads, int(threads_number))
     # output bwt
     bwt.saveAsTextFile(output_path)
     # reverse bwt to reads
-    #check(bwt, reads.count())
+    check(bwt, reads.count())
     
     finish_time = datetime.now()
     print finish_time-start_time
