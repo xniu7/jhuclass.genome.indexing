9ae72a2472bb35c1e06c3db4164cd76bf1c5dff3
xniu7 <xniu7@jhu.edu>
2014-07-06 23:49:08 -0400

    v 1.0.0

--- code/test.sh
+++ code/test.sh
@@ -4,5 +4,5 @@ if [ $1 = "pbpr" ]; then
 	../spark/bin/pyspark pbpr.py spark://$2:7077 s3n://fsda/data/$3/input s3n://fsda/data/$3/output
 # run radix sort, segment sort or default sort
 else
-	../spark/bin/pyspark sort.py $1 spark://$2:7077 $3 s3n://fsda/data/$4/input s3n://fsda/data/$4/output
+  ../spark/bin/spark-submit sort.py $1 spark://$2:7077 $3 $4 s3n://ss-reads/$5 s3n://ss-reads/$5_output
 fi
